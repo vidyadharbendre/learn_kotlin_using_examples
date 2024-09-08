91d58994f950ff117c61f04bd46d0004efab5053
vidyadharbendre <vidyadhar.bendre@gmail.com>
2024-09-08 18:27:27 +0530

    added class example in kotlin

---
+++ kotlin/src/main/day_3_classes_objects/pgm1_classes/pgm1_classes_example.kt
@@ -0,0 +1,35 @@
+package pgm1_classes
+
+/*
+ * Program     : SimpleClassDemo.kt
+ * Author      : Vidyadhar Bendre
+ * Date        : 8-September-2024
+ *
+ * Significance:
+ * This program demonstrates a simple class in Kotlin.
+ * The `Person` class contains properties and a method to print details.
+ * It serves as an introductory example of creating and using classes in Kotlin.
+ *
+ * Note:
+ * This code can be run in the Kotlin Playground at https://play.kotlinlang.org/.
+ */
+
+// Simple class with properties and a method
+class Person(val name: String, var age: Int) {
+    fun printDetails() {
+        println("Name: $name, Age: $age")
+    }
+}
+
+// Main function: Entry point of the Kotlin program
+fun main() {
+    val personRefObj = Person("Vidyadhar", 54)
+    personRefObj.printDetails()
+}
+
+/*
+ * Output of the program:
+ *
+ * Name: Vidyadhar, Age: 54
+ *
+ */
---
+++ out/production/learn_kotlin_using_examples/pgm2_variable_types/pgm2_variable_types_README.md
@@ -0,0 +1,15 @@
+# Kotlin Program: Variable Types Demonstration
+
+## Author
+**Vidyadhar Bendre**  
+**Date:** 31-August-2024
+
+## Program Overview
+This program demonstrates the basics of Kotlin variables, showcasing how to declare, initialize, and modify variables using the `var` keyword. It illustrates the mutability of variables, allowing changes to their values after their initial assignment. The program uses simple data types like `Int` and `String` to highlight Kotlin's type inference.
+
+## Program Explanation
+- The program initially sets an integer variable `age` to 5 and a string variable `name` to "Kotlin".
+- It prints the initial values of `age` and `name`.
+- The variables are then reassigned new values: `age` is set to 6, and `name` is changed to "Android".
+- The program prints the modified values, demonstrating Kotlin's mutability using `var`.
+
